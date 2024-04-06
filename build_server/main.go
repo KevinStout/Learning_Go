87ccc4033359ea730839b752e69b1c37a7e27506
Kevin Stout <stoutkev3@gmail.com>
2024-04-06 00:00:40 -0400

    added a query param to the get request

--- build_server/main.go
+++ build_server/main.go
@@ -3,9 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"example.com/greetings"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	// Get the "name" query param from the URL.
+	name := r.URL.Query().Get("name")
+	// If no name was given in the query param, use "Guest" as the default name.
+	if name == "" {
+		name = "Guest"
+
+	}
+
+	greeting, err := greetings.Hello(name)
+	if err != nil {
+		fmt.Fprintf(w, "%s", err)
+		return
+	}
+	fmt.Fprintf(w, "%s", greeting)
 	fmt.Fprintf(w, "Hello, Mom! %s", r.URL.Path)
 }
 
